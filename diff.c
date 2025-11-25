5b0a5c1649dd6f04002138557ca9b776eaa55ec8
Roshan <gungsingh404@gmail.com>
2025-11-25 10:45:33 +0530

    initial commit

---
+++ diff.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a, b ,diff;
+    a = 4;
+    b = 3;
+    diff = a - b;
+    printf("diff : %d",diff);
+
+    return 0;
+    }
---
+++ sum.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a:");
+    printf("enter b:");
+    
